72381270bfd4bb19b8c6b370fd7498ba011e0c96
rowjah <124186777+rowjah@users.noreply.github.com>
2023-02-13 10:37:15 +0530

    p change

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -13,8 +13,8 @@ const Home = () => {
   <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
     <div className='flex flex-col justify-center h-full'>
         <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a frontend developer</h2>
-        <p className='text-gray-700 py-4 '>Adaptable to new technology to enchance my skills and capabilities.Adaptable to new technology to enchance my skills and capabilities
-        Adaptable to new technology to enchance my skills and capabilities Adaptable to new technology to enchance my skills and capabilities</p>
+        <p className='text-gray-700 py-4 '>Adaptable to new technology to enchance my skills and capabilities.I believe that my technical,
+         functional and communication skills will enable me in facing the challenging career ahead and productivity gains to the organization.</p>
         <div>
             <Link to="portfolio" smooth duration={500}  className=' group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                 portfolio
