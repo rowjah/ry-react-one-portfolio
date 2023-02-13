15f4b459840c41489a0da5e878f1cdcadb5c3572
rowjah <124186777+rowjah@users.noreply.github.com>
2023-02-13 10:41:10 +0530

    p change

--- src/Component/Experience.jsx
+++ src/Component/Experience.jsx
@@ -64,12 +64,14 @@ const Experience = () => {
 
 
     ]
+
   return (
 
 <div name="skills" className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
         <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
             <div>
                 <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Skills</p>
+
                 <p className='py-6'>These are the technologies i've worked with</p>
             </div>
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
@@ -81,7 +83,7 @@ const Experience = () => {
                   <p className='mt-4'>{title}</p>
               </div>
 
-              ))  }
+              ))  } 
 
 
 
