99f8e257be1c337afdd0fc7c8acc3c445707da18
rowjah <124186777+rowjah@users.noreply.github.com>
2023-02-13 10:39:11 +0530

    tag change

--- src/Component/Portfolio.jsx
+++ src/Component/Portfolio.jsx
@@ -32,7 +32,7 @@ const Portfolio = () => {
                  <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
                      <img src={src} alt='' className='rounded-md duration-200 hover:scale-105'  />
                      <div className='flex items-center justify-center'>
-                         <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+                         <button  className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
                          <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
                      </div>
                  </div>
